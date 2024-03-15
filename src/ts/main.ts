78baf16942a5457f1df5caa27b85ffacf77b9094
leonarddeutgen <leonard.deutgen@medieinstitutet.se>
2024-03-15 12:33:47 +0100

    first commit

---
+++ src/ts/main.ts
@@ -0,0 +1,16 @@
+import "../sass/style.css";
+
+const headerContainer = document.getElementById("header");
+const mainContainer = document.getElementById("main");
+
+const mainCreateHTML = () => {
+  const createTicketBtn = document.createElement("button");
+
+  createTicketBtn.className = "createTicketBtn";
+
+  createTicketBtn.innerHTML = "Create new ticket";
+
+  headerContainer?.appendChild(createTicketBtn);
+};
+
+mainCreateHTML();
